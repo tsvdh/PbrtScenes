404dbadbc1ba1d5d7bf01b6d787e4b02a6546338
Tobias van den Hurk <tsvdhurk@gmail.com>
2025-01-02 13:39:37 +0100

    add subdivision results

---
+++ cube/subdivisions/precompute.bat
@@ -0,0 +1,39 @@
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_base.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad01.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad02.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim100_d5_rad01.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim100_d5_rad02.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim30_d5_rad02.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim30_d5_rad03.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim30_d5_rad04.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad03.json
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad04.json
---
+++ cube/subdivisions/render.bat
@@ -0,0 +1,49 @@
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_base.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim30_d5_rad02.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim30_d5_rad03.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim30_d5_rad04.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad01.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad02.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad03.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad04.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim100_d5_rad01.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
+
+C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+--graph-config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim100_d5_rad02.json ^
+--display-server localhost:14158 --graph-render-radius-mod 20
