c200635f9eeb20f1e9b98c622731d7a3452efdb1
Tobias van den Hurk <tsvdhurk@gmail.com>
2025-01-09 09:22:46 +0100

    new results for meeting

--- cube/subdivisions/precompute.bat
+++ cube/subdivisions/precompute.bat
@@ -30,10 +30,10 @@
 @REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
 @REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim30_d5_rad04.json
 
-@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
-@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
-@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad03.json
-
 C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
 C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
---config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad04.json
+--config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad03.json
+
+@REM C:\Users\tsvdh\CodeProjects\Clion\AcceleratedVolRenderer\build\Release\graph_maker.exe ^
+@REM C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\cube.pbrt ^
+@REM --config C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\cube\subdivisions\cube_dim50_d5_rad04.json
