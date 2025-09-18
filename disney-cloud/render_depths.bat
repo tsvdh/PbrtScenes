dee554abfb1b86994238ca98a166eb650006b47a
Tobias van den Hurk <tsvdhurk@gmail.com>
2025-09-18 10:49:20 +0200

    push

---
+++ disney-cloud/render_depths.bat
@@ -0,0 +1,24 @@
+CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud.pbrt ^
+--display-server localhost:14158 --maxdepth 1 ^
+--graph-data C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud_d1.txt
+
+CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud.pbrt ^
+--display-server localhost:14158 --maxdepth 2 ^
+--graph-data C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud_d2.txt
+
+CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud.pbrt ^
+--display-server localhost:14158 --maxdepth 3 ^
+--graph-data C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud_d3.txt
+
+CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud.pbrt ^
+--display-server localhost:14158 --maxdepth 5 ^
+--graph-data C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud_d5.txt
+
+CodeProjects\Clion\AcceleratedVolRenderer\build\Release\pbrt.exe ^
+C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud.pbrt ^
+--display-server localhost:14158 --maxdepth 10 ^
+--graph-data C:\Users\tsvdh\CodeProjects\VSCode\PbrtScenes\disney-cloud\disney-cloud_d10.txt
